e7cb4e735e795316df12732820f3e929af0f882e
PraZ <sachabouillez@gmail.com>
2022-06-06 13:26:58 +0200

    feat: move content to private submodule

--- components/content/CongressForm.vue
+++ components/content/CongressForm.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <img src="~/assets/svgs/" alt="">
+  <div bg-red-100 p-2 shadow-sm sm:px-6>
+    <h2>{{ "S'inscrire au congrès" }}</h2>
+    <p><slot name="description" /></p>
+    <form flex flex-col space-y-2>
+      <div flex flex-col space-y-2 sm:flex-row sm:space-y-0>
+        <FormInput label="Prénom" name="firstName" />
+        <FormInput label="Nom" name="lastName" />
+      </div>
+    </form>
   </div>
 </template>
--- pages/test.vue
+++ pages/test.vue
@@ -1,3 +1,6 @@
 <template>
-  <FormInput label="My field" name="custom-field" />
+  <FormInput label="My field" name="custom-field" error="oops" />
+  <FormInput label="My field 2" name="custom-field2" />
+  <FormSelect label="My select" name="my-select" />
+  <FormCheckbox label="My checkbox" name="my-checkbox" />
 </template>
